c79f50484cf7abfb3fa74f73032ebf636f5c8761
kushan-ghughu <kushanghughu25@gmail.com>
2025-07-03 21:10:19 +0530

    project complete

--- data/products.js
+++ data/products.js
@@ -8,6 +8,57 @@ export function getProduct(productId) {
   return matchingProduct;
 }
 
+class Product{
+  id;
+  image;
+  rating;
+  priceCents;
+
+  constructor (productDetails){
+    this.id = productDetails.id;
+    this.name = productDetails.name;
+    this.image = productDetails.image;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+  extraInfoHtml(){
+    return ``
+  }
+};
+
+class Clothing extends Product{
+  sizeChartLink;
+  constructor (productDetails){
+      super(productDetails);
+      this.sizeChartLink= productDetails.sizeChartLink;
+  }
+// method overwriting 
+  extraInfoHtml(){
+    return `
+      <a href="${this.sizeChartLink}" target="_blank" >Size Chart</a>
+    `
+  }
+  
+};
+
+
+// not consider this method lenagthy process
+// const product1 = new Product(
+//    {
+//     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+//     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+//     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+//     rating: {
+//       stars: 4.5,
+//       count: 87,
+//     },
+//     priceCents: 1090,
+//     keywords: ["socks", "sports", "apparel"],
+//   }
+// );
+//console.log(product1)
+
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -479,4 +530,12 @@ export const products = [
     priceCents: 2400,
     keywords: ["sweaters", "hoodies", "apparel", "mens"],
   },
-];
+].map((productDetails)=>{
+  if(productDetails.type == "clothing"){
+      return new Clothing(productDetails);
+  }else{
+      return new Product(productDetails);
+  }
+}
+);
+console.log(products);
